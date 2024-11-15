be71eedb519977a02ff48d5e64e7ba5c4c036414
Anh Nguyet Vu <anngvu@gmail.com>
2024-11-14 16:48:54 -0800

    More test updates

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,12 +1,12 @@
 # Implementing skips according to suggested handling when using reticulate
 # See https://rstudio.github.io/reticulate/articles/package.html
 # Skips tests on CRAN machines or other incompatible testing environments
-# where Python can't be configured so package checks don't fail 
+# where Python can't be configured so package checks don't fail
 
 # Skip if Python synapseclient module not installed/accessible
 # This is normally imported upon package load, see `zzz.R`
 skip_if_no_synapseclient <- function() {
-  have_synapseclient <- py_module_available("synapseclient") 
+  have_synapseclient <- py_module_available("synapseclient")
   if(!have_synapseclient)
     skip("synapseclient not available for testing")
 }
@@ -14,14 +14,14 @@ skip_if_no_synapseclient <- function() {
 # Skip if Python synapseutils module not installed/accessible
 # This is normally imported upon package load, see `zzz.R`
 skip_if_no_synapseutils <- function() {
-  have_synapseutils <- py_module_available("synapseclient") 
+  have_synapseutils <- py_module_available("synapseclient")
   if(!have_synapseutils)
     skip("synapseutils not available for testing")
 }
 
 # Skip if no pandas; pandas is needed for smaller subset of functions in the package
 skip_if_no_pandas <- function() {
-  have_pandas <- py_module_available("pandas") 
+  have_pandas <- py_module_available("pandas")
   if(!have_pandas)
     skip("pandas not available for testing")
 }
@@ -37,6 +37,6 @@ skip_if_no_token <- function() {
 # (e.g. someone pasted in wrong token), this creates a skip cascade for tests that presume
 # successful login.
 skip_if_no_login <- function() {
-  if(!exists(".syn"))
+  if(!exists(".syn") || is.null(.syn$username))
     skip("not logged in for tests")
 }
---
+++ tests/testthat/test_manifest_annotations.R
@@ -0,0 +1,8 @@
+test_that("Annotate with manifest works", {
+  skip_if_no_synapseclient()
+  skip_if_no_login()
+
+  PARENT_TEST_PROJECT <- "syn26462036"
+  testthat::expect_equal(1,1)
+
+})
